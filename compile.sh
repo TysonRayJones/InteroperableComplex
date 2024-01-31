5daa9b5ee31e3b15e262842e8febbd39e20a2ada
TysonRayJones <tyson.jones.input@gmail.com>
2024-01-31 22:04:42 +0100

    can i do dis

--- compile.sh
+++ compile.sh
@@ -1,26 +1,36 @@
 # terminate upon error
 set -e
 
-echo "compiling CPU backend in C++..."
-g++ -c src/core.cpp -Isrc -std=c++14 -o core.o
-g++ -c src/alts.cpp -Isrc -std=c++14 -o alts.o
+cflags='-Isrc -std=c99'
+cppflags='-Isrc -std=c++14'
+cudaflags='-Isrc -std=c++14'
 
-echo "compiling CPU main in C++..."
-g++ main.cpp core.o alts.o -Isrc -std=c++14 -o main_cpp
+echo "compiling CPU backend in C++..."
+g++ -c src/core.cpp $cppflags -o core.o
+g++ -c src/alts.cpp $cppflags -o alts.o
+
+echo "compiling and linking CPU main in C++..."
+g++ main.cpp core.o alts.o $cppflags -o main_cpp
 ./main_cpp
 
-echo "compiling CPU main in C..."
-gcc main.c core.o alts.o -Isrc -std=c99 -o main_c
+echo "compiling and linking CPU main in C..."
+gcc main.c core.o alts.o $cflags -o main_c
 ./main_c
 
 echo "compiling GPU backend in C++..."
-nvcc -c src/core.cu -Isrc -std=c++14 -o core.o
-g++ -c src/alts.cpp -Isrc -std=c++14 -o alts.o
+nvcc -c src/core.cu $cudaflags -o core.o
+g++ -c src/alts.cpp $cppflags -o alts.o
 
 echo "compiling GPU main in C++..."
-g++ main.cpp core.o alts.o -Isrc -std=c++14 -o main_cpp
-./main_cpp
+g++ -c main.cpp $cppflags -o main.o
+
+echo "linking GPU C++..."
+nvcc main.o core.o alts.o $cudaflags -o main_cuda_cpp
+./main_cuda_cpp
 
 echo "compiling GPU main in C..."
-gcc main.c core.o alts.o -Isrc -std=c99 -o main_c
-./main_c
+gcc -c main.c $cflags -o main.o
+
+echo "linking GPU C..."
+nvcc main.o core.o alts.o $cudaflags -o main_cuda_c
+./main_cuda_c
